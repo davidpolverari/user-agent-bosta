0fafd3c1f8dc71c6cf0ed3d4a72bdf0429f31c28
David Polverari <david.polverari@gmail.com>
2020-04-18 19:14:46 -0400

    Commit inicial

---
+++ README.md
@@ -0,0 +1,9 @@
+# user-agent-bosta
+
+## INSTALACAO
+
+* Editar a variavel ```ua``` no arquivo ```background.js```
+* Colocar ```chrome://extensions/``` no Chrome/Chromium
+* Clicar em 'Load Unpacked'
+* Selecionar o diretorio baixado
+* Habilitar a extensao se nao tiver sido habilitarda 
---
+++ background.js
@@ -0,0 +1,16 @@
+"use strict";
+
+var ua = "Merda (1.2.3)";
+
+function rewriteUserAgentHeader(e) {
+  for (var header of e.requestHeaders) {
+    if (header.name.toLowerCase() === "user-agent") {
+      header.value = ua;
+    }
+  }
+  return {requestHeaders: e.requestHeaders};
+}
+
+chrome.webRequest.onBeforeSendHeaders.addListener(rewriteUserAgentHeader,
+                                          {urls: ['https://*/*', "http://*/*"]},
+                                          ["blocking", "requestHeaders"]);
