05ca8c49e433df9cd93774c65f5c2e857b3716fa
MARCOS V SCHERER <marcos.v.scherer@gmail.com>
2020-04-19 01:25:23 -0300

    Adicionado suporte a storage e ajustado em um popup.

--- background.js
+++ background.js
@@ -1,18 +1,33 @@
 "use strict";
 
-var ua = "Merda (1.2.3)";
-
-function rewriteUserAgentHeader(e) {
-  for (var header of e.requestHeaders) {
-    if (header.name.toLowerCase() === "user-agent") {
-      header.value = ua;
-    }
-  }
-  return {requestHeaders: e.requestHeaders};
-}
-
 var API = chrome || browser;
 
-API.webRequest.onBeforeSendHeaders.addListener(rewriteUserAgentHeader,
-                                          {urls: ['<all_urls>']},
-                                          ["blocking", "requestHeaders"]);
+function pegarStorageUA() {
+		API.storage.sync.get("ua", function(result) {
+			if (result.ua == undefined) {
+				pegarStorageUA.ua = navigator.userAgent;
+			} else {
+				pegarStorageUA.ua = result.ua;
+			}
+		});
+}
+
+function rewriteUserAgentHeader(e) {
+	for (var header of e.requestHeaders) {
+		if (header.name.toLowerCase() === "user-agent") {
+			header.value = pegarStorageUA.ua;
+			console.log('Definius::'+ header.value);
+		}
+	}
+	return {requestHeaders: e.requestHeaders};
+}
+
+pegarStorageUA();
+
+API.storage.onChanged.addListener(pegarStorageUA);
+
+API.webRequest.onBeforeSendHeaders.addListener(
+	rewriteUserAgentHeader,
+	{urls: ['<all_urls>']},
+	["blocking", "requestHeaders"]
+);
